16d52bdb670a62676361e8037cfb960707511bf7
megamaced <22581049+megamaced@users.noreply.github.com>
2026-04-12 20:15:31 +0000

    feat: app skeleton scaffolding

---
+++ appinfo/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'routes' => [
+		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+		['name' => 'page#index', 'url' => '/{path}', 'verb' => 'GET', 'requirements' => ['path' => '.+']],
+	],
+	'ocs' => [
+		['name' => 'media#index',   'url' => '/api/v1/media',      'verb' => 'GET'],
+		['name' => 'media#show',    'url' => '/api/v1/media/{id}', 'verb' => 'GET'],
+		['name' => 'media#create',  'url' => '/api/v1/media',      'verb' => 'POST'],
+		['name' => 'media#update',  'url' => '/api/v1/media/{id}', 'verb' => 'PUT'],
+		['name' => 'media#destroy', 'url' => '/api/v1/media/{id}', 'verb' => 'DELETE'],
+	],
+];
